d378741372729f2a9c38e6b88e5a89d1adc0700b
minhaz1010 <minhazrahman1010@gmail.com>
2024-08-30 14:31:01 +0600

    removing all console log

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,9 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import AppRoutes from './routes/AppRoutes';  // Import the new AppRoutes component
 import { persistor, store } from './redux/store';
-import { ReactNotifications } from 'react-notifications-component'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <ReactNotifications />
       <PersistGate loading={null} persistor={persistor}>
         <AppRoutes />
       </PersistGate>
