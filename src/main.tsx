0aa28e6783518e169f8ff819f2d674623bec88c8
minhaz1010 <minhazrahman1010@gmail.com>
2024-09-01 13:58:39 +0600

    add title

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-// src/main.tsx
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import AppRoutes from './routes/AppRoutes';  // Import the new AppRoutes component
+import AppRoutes from './routes/AppRoutes';
 import { persistor, store } from './redux/store';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
