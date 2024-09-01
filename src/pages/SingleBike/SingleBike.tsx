1c58d0b997a25947549cd47e87b6c3ff64b3fc18
minhaz1010 <minhazrahman1010@gmail.com>
2024-09-01 21:09:38 +0600

    add vercel.json and add taka in single bike price per hour section

--- src/pages/SingleBike/SingleBike.tsx
+++ src/pages/SingleBike/SingleBike.tsx
@@ -53,7 +53,7 @@ const SingleBike = () => {
           <h3 className='text-3xl md:text-4xl lg:text-5xl text-green-500 '>{bike.name}</h3>
           <p className='text-lg md:text-xl'>{bike.description}.</p>
           <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
-            <p className='text-lg md:text-xl font-light'>Price Per Hour <span className='block text-2xl md:text-3xl text-green-500'>${bike.pricePerHour}</span></p>
+            <p className='text-lg md:text-xl font-light'>Price Per Hour <span className='block text-2xl md:text-3xl text-green-500'>{bike.pricePerHour} taka</span></p>
             <p className='text-lg md:text-xl font-light'>Engine Power <span className='block text-2xl md:text-3xl text-green-500'>{bike.cc}</span></p>
           </div>
           <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
